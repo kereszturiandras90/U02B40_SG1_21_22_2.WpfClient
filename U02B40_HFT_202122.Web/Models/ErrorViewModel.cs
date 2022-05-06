12dc538902a8e3f602af82cdd12579b889ca6fd8
LAPTOP-M24PTUQR\keres <kereszturiandras90@gmail.com>
2022-05-06 15:09:57 +0200

    Do all changes

---
+++ U02B40_HFT_202122.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace U02B40_HFT_2021221.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
